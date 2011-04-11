398588fb1ba8e8dffa37b9756771bc9c467cbca0
Reis <reis89@freenet.de>
2011-04-11 16:43:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '0a5e7e8424cf3f6efe7dffe9f2b9017dfe390a56ddedc0c8d00c708ac5ed5468ae2758fd5d228b8f5c9a18698a9faa3d9cd8dfaf46ffb255b826cc729509a15d'
